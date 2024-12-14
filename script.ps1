5ba41cdb8b678bc9ade8b7327311e712998178a3
arslanbyte <56575500+arslanbyte@users.noreply.github.com>
2024-12-14 15:52:00 +0500

    Update and rename script to script.ps1

--- script.ps1
+++ script.ps1
@@ -2,7 +2,7 @@ $RootDirectoryToCheck = 'C:\Path\To\InputFiles'
 $DestinationDirectory = 'C:\Path\To\Found'
 
 Import-Csv 'C:\Path\To\Names.csv' | ForEach-Object {
-    $TextToFind = $_.Antennas
+    $TextToFind = $_.YourFieldName
 
     foreach ($file in Get-ChildItem -Path $RootDirectoryToCheck -Filter '*.raf' -File -Recurse -OutVariable csvFiles) {
         Select-String $file -Pattern $TextToFind | Select-Object -Property Path, Line | Export-Csv 'C:\Path\To\results.csv' -Append
